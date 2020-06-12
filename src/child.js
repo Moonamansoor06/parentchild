08b0f39e69c985d08eedc1fa0cbf8527aa29c7ea
Moona <moonamansoor06@gmail.com>
2020-06-12 13:48:55 +0500

    Update child.js

--- src/child.js
+++ src/child.js
@@ -3,16 +3,13 @@ import React from "react"
 function Child(props){
 
         return (
-            <div id= "childdiv" style={{ background: 'blue' }}>
+            <div id= "childdiv" style={{ background: 'green' }}>
                
-      
-      
+
         <p>Student Name : {props.studentName}</p>
         
         <p>Student Age : {props.studentAge}</p>
-        <p> Student Address : {props.studentAddress}</p>
-
-                
+        <p> Student Address : {props.studentAddress}</p>  
   
             </div>
         )
